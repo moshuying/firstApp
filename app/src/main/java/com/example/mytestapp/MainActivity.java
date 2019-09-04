1e299391f1181890db180c2f147cb16a43fcfcb8
moshuying <1460083332@qq.com>
2019-09-04 18:29:31 +0800

    初学者很难受

--- app/src/main/java/com/example/mytestapp/MainActivity.java
+++ app/src/main/java/com/example/mytestapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -15,6 +16,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.gson.Gson;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -22,13 +24,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-
+    public static String result;
+    private Button btn;//点击按钮访问
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,8 +68,50 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        btn = findViewById(R.id.clickToChangeMessage);//绑定ID
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {//监听按钮
+                new Thread(new Runnable() {//创建子线程
+                    @Override
+                    public void run() {
+                        getwebinfo();//把路径选到MainActivity中
+                    }
+                }).start();//启动子线程
+            }
+        });
     }
+    private void getwebinfo() {
+        try {
+            //1,找水源--创建URL
+            URL url = new URL("https://www.baidu.com/");//放网站
+            //2,开水闸--openConnection
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            //3，建管道--InputStream
+            InputStream inputStream = httpURLConnection.getInputStream();
+            //4，建蓄水池蓄水-InputStreamReader
+            InputStreamReader reader = new InputStreamReader(inputStream, "UTF-8");
+            //5，水桶盛水--BufferedReader
+            BufferedReader bufferedReader = new BufferedReader(reader);
 
+            StringBuffer buffer = new StringBuffer();
+            String temp = null;
+
+            while ((temp = bufferedReader.readLine()) != null) {
+                //取水--如果不为空就一直取
+                buffer.append(temp);
+            }
+            bufferedReader.close();//记得关闭
+            reader.close();
+            inputStream.close();
+            Log.e("MAIN",buffer.toString());//打印结果
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -70,7 +125,63 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
     }
     public void changeMainButton(View view){
+//        Gson gson = new Gson();
+//        ShopInfo shopInfo = gson.fromJson(json, ShopInfo.class);
         TextView textView = findViewById(R.id.textView2);
         textView.setText(EXTRA_MESSAGE);
+    }
+    public void show(View view){
+        String json = "[\n" +
+                "    {\n" +
+                "        \"id\": 1,\n" +
+                "        \"imagePath\": \"http://blog.csdn.net/qq_29269233/f1.jpg\",\n" +
+                "        \"name\": \"金鱼1\",\n" +
+                "        \"price\": 12.3\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": 2,\n" +
+                "        \"imagePath\": \"http://blog.csdn.net/qq_29269233/f2.jpg\",\n" +
+                "        \"name\": \"金鱼2\",\n" +
+                "        \"price\": 12.5\n" +
+                "    }\n" +
+                "]";
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText(json);
+    }
+    public void show(String json){
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText(json);
+    }
+    public void send(View view) {
+        try {
+            //1,找水源--创建URL
+            URL url = new URL("https://www.baidu.com/");//放网站
+            //2,开水闸--openConnection
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            //3，建管道--InputStream
+            InputStream inputStream = httpURLConnection.getInputStream();
+            //4，建蓄水池蓄水-InputStreamReader
+            InputStreamReader reader = new InputStreamReader(inputStream, "UTF-8");
+            //5，水桶盛水--BufferedReader
+            BufferedReader bufferedReader = new BufferedReader(reader);
+
+            StringBuffer buffer = new StringBuffer();
+            String temp = null;
+
+            while ((temp = bufferedReader.readLine()) != null) {
+                //取水--如果不为空就一直取
+                buffer.append(temp);
+            }
+            bufferedReader.close();//记得关闭
+            reader.close();
+            inputStream.close();
+            TextView textView = findViewById(R.id.textView2);
+            textView.setText(buffer.toString());
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
